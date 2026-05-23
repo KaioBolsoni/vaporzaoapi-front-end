466e8c925060b45548ae6dac121030a0a10f11f0
kaiobolsoni <kaiobolsonioriginal@gmail.com>
2026-05-23 12:56:26 -0300

    feat: implement authentication flow with protected routes and base CSS styling

--- vaporzao/src/services/api.js
+++ vaporzao/src/services/api.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://alunos-ads-api-production.up.railway.app', 
+  baseURL: 'https://alunos-ads-api-production.up.railway.app',
+});
 
-  headers: {
-    'Content-Type': 'application/json'
+api.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = {token};
   }
+
+  return config;
 });
 
 export default api;
