65264080e8905223898eba7665297a65a789ebeb
kaiobolsoni <kaiobolsonioriginal@gmail.com>
2026-05-23 12:58:01 -0300

    feat: implement authentication flow with protected routes and base global styles

--- vaporzao/src/App.jsx
+++ vaporzao/src/App.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-
+import ProtectedRoute from "./components/ProtectRouter/ProtectedRoute";
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
