e2292f2aa58b714e70fcbe654a780c435aaac8b3
Thales Caetano <thalesfercaetano@gmail.com>
2026-05-28 21:30:15 -0300

    feat: add routes for /catalogo, /jogos/:id, and /perfil/:matricula

--- vaporzao/src/App.jsx
+++ vaporzao/src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import FirstAccess from "./pages/FirstAccess";
+import Catalog from "./pages/Catalog";
+import GameDetails from "./pages/GameDetails";
+import PublicProfile from "./pages/PublicProfile";
 import ProtectedRoute from "./components/ProtectRouter/ProtectedRoute";
 
 function App() {
@@ -15,6 +18,30 @@ function App() {
         element={
           <ProtectedRoute>
             <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/catalogo"
+        element={
+          <ProtectedRoute>
+            <Catalog />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/jogos/:id"
+        element={
+          <ProtectedRoute>
+            <GameDetails />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/perfil/:matricula"
+        element={
+          <ProtectedRoute>
+            <PublicProfile />
           </ProtectedRoute>
         }
       />
